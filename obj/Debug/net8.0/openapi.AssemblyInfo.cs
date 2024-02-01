d3f80fb12b41784cdb5f4d5b90a0c881577b2d60
njaci1 <githatukelvin@gmail.com>
2024-02-01 14:49:55 +0300

    Autogenerated update by MSBuild

--- obj/Debug/net8.0/openapi.AssemblyInfo.cs
+++ obj/Debug/net8.0/openapi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("openapi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b74f2a49369c68549098f001de123ef53a529032")]
 [assembly: System.Reflection.AssemblyProductAttribute("openapi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("openapi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
